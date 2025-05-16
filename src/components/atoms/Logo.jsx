cb9a82f541bbf27665396e6e682c998c526fc646
sangleb <sanglesada2@gmail.com>
2025-05-16 19:41:38 +0530

    logo src modified

--- src/components/atoms/Logo.jsx
+++ src/components/atoms/Logo.jsx
@@ -5,8 +5,8 @@ function Logo({width, height}){
     return (
         <div className="w-32 px-5 mb-5">
             <img 
-                src="/src/assets/image.png"
-                alt={LogoImage}
+                src={LogoImage}
+                alt="Logo"
                 className={`w-${width} h-${height}`}
             />
         </div>
